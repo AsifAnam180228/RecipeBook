72a9738272a6554e0123379d03798795a151cd91
AsifAnam180228 <anam1828@cseku.ac.bd>
2023-08-07 14:43:30 +0600

    udemy video 60

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SplitButtonModule } from 'primeng/splitbutton';
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent {
+
+}
